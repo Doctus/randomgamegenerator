64b0f3ea39689efca7dd48dd401707a8b669e111
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2009-06-24 18:33:36 +0000

    assuming the merge worked (and no typoes), tells now possible

--- trunk/scripts/test3.py
+++ trunk/scripts/test3.py
@@ -22,6 +22,17 @@ def newEvent(str):
             c.insertChatMessage('<i>' + c.getLocalHandle() + ' ' +
                                 unicode(action) + '</i>')
             c.sendNetMessageToAll('T!' + unicode(action))
+        elif words[0] == '/w' or '/t' or '/whisper' or '/tell':
+            if len(words) == 1:
+                mesg = ("Syntax: /whisper HANDLE MESSAGE. Sends a message " +
+                        "only to the specified user. Spaces MUST be correct.")
+                c.insertChatMessage(unicode(mesg))
+            else:
+                target = words[1]
+                mesg = words[2:]
+                c.insertChatMessage('To ' + unicode(target) + ': ' +
+                                    unicode(mesg))
+                c.sendNetMessageToHandle('w!' + unicode(mesg), target)
     else:
         c.insertChatMessage(c.getLocalHandle() + ": " + unicode(str))
         c.sendNetMessageToAll("t!" + unicode(str))
@@ -34,6 +45,9 @@ def newNetEvent(str, handle):
             elif str[0] == 'T': #Emote message
                 c.insertChatMessage('<i>' + unicode(handle) + " " +
                                     unicode(str[2:]) + '</i>')
+            elif str[0] == 'w': #Whisper/tell
+                c.insertChatMessage('From ' + unicode(handle) + ': ' +
+                                    unicode(str[2:]))
             elif str[0] == 'n': #Map file
                 #This isn't useful, just demonstrating principle
                 loadedmappe = rggTileLoader.loadFromString(str[2:])
