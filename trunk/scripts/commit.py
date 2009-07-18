0023d8346f94148b98aa6c777e511d19e225b3c6
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2009-07-18 00:26:23 +0000

    Edited various assorted features and mostly worked on old functions.

---
+++ trunk/scripts/commit.py
@@ -0,0 +1,19 @@
+#Random commit comment generator. By Doctus.
+import random
+
+verbs = ['added', 'improved', 'enhanced', 'fixed', 'repaired', 'removed', 'deleted', 'edited', 'modified', 'expanded', 'worked on']
+adjs = ['helpful', 'assorted', 'various', 'several', 'GUI', 'network', 'backend', 'frontend', 'logic', 'processing', 'old']
+nouns = ['bugs', 'features', 'routines', 'functions', 'signals', 'objects', 'classes', 'interfaces', 'widgets']
+
+def getCommitComment():
+  patterns = [[random.choice(verbs), random.choice(adjs), random.choice(nouns)],
+            [random.choice(verbs), random.choice(nouns), "and", random.choice(verbs), random.choice(adjs),
+             random.choice(nouns)],
+            [random.choice(verbs), random.choice(adjs), random.choice(adjs), random.choice(nouns), "and",
+             random.choice(verbs), random.choice(adjs), random.choice(nouns)],
+              [random.choice(verbs), random.choice(adjs), random.choice(adjs), random.choice(nouns), "and mostly",
+             random.choice(verbs), random.choice(adjs), random.choice(nouns)]]
+  return (" ".join(random.choice(patterns))).capitalize() + "."
+
+for r in range(0, 10):
+  print getCommitComment()
