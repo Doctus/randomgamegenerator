07facb73b8a3146e365c7d4e12905afc09be0d5a
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2009-08-12 15:21:21 +0000

    What happen. Somebody set up us the conflict.

--- trunk/scripts/rgg_py_ja.ts
+++ trunk/scripts/rgg_py_ja.ts
@@ -1,30 +1,60 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="ja_JP">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>@default</name>
+    <message>
+        <source>Dice</source>
+        <translation>サイコロ</translation>
+    </message>
+    <message>
+        <source>Add Macro</source>
+        <translation>サイコロマクロを作る</translation>
+    </message>
+    <message>
+        <source>Delete Macro</source>
+        <translation>サイコロマクロを消す</translation>
+    </message>
+</context>
+<context>
+    <name>QtCore.QObject</name>
+    <message>
+        <source>Dice</source>
+        <translation>サイコロ</translation>
+    </message>
+</context>
 <context>
     <name>c</name>
     <message>
-        <source>Please type &amp;#38;#60; if you wish to include &amp;#60; in your message.</source>
-        <translation type="unfinished"></translation>
+        <source> rolls </source>
+        <translation>は</translation>
     </message>
     <message>
-        <source> rolls </source>
-        <translation type="unfinished"></translation>
+        <source>Please type &amp;#38;#60; if you wish to include &amp;#60; in your message.</source>
+        <translation>OBSOLETE STRING</translation>
     </message>
 </context>
 <context>
     <name>diceRoller</name>
     <message>
-        <source>Dice</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Add Macro</source>
-        <translation type="unfinished"></translation>
+        <translation>サイコロマクロを作る</translation>
     </message>
     <message>
         <source>Delete Macro</source>
-        <translation type="unfinished"></translation>
+        <translation>サイコロマクロを消す</translation>
+    </message>
+    <message>
+        <source>Dice</source>
+        <translation>サイコロ</translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <source>Dice</source>
+        <translation>サイコロ</translation>
     </message>
 </context>
 </TS>
