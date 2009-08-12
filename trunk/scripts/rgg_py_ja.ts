b065d6ab69c62f480299e68d4ab49ea38e36cc5e
kingoipo <kingoipo@4e94e682-459d-11de-b3c2-97ef4ed79301>
2009-08-12 15:05:24 +0000

    Removed obsolete translations from rgg_py_ja.ts

--- trunk/scripts/rgg_py_ja.ts
+++ trunk/scripts/rgg_py_ja.ts
@@ -1,57 +1,30 @@
-<!DOCTYPE TS><TS>
-<context>
-    <name>@default</name>
-    <message>
-        <source>Dice</source>
-        <translation type="obsolete"></translation>
-    </message>
-    <message>
-        <source>Add Macro</source>
-        <translation type="obsolete"></translation>
-    </message>
-    <message>
-        <source>Delete Macro</source>
-        <translation type="obsolete"></translation>
-    </message>
-</context>
-<context>
-    <name>QtCore.QObject</name>
-    <message>
-        <source>Dice</source>
-        <translation type="obsolete"></translation>
-    </message>
-</context>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
 <context>
     <name>c</name>
     <message>
-        <source> rolls </source>
+        <source>Please type &amp;#38;#60; if you wish to include &amp;#60; in your message.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Please type &amp;#38;#60; if you wish to include &amp;#60; in your message.</source>
+        <source> rolls </source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>diceRoller</name>
     <message>
+        <source>Dice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>Add Macro</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Delete Macro</source>
         <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Dice</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>mainWindow</name>
-    <message>
-        <source>Dice</source>
-        <translation type="obsolete"></translation>
     </message>
 </context>
 </TS>
