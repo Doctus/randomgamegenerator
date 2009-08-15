838d64c26a92d158ed8d9eccf08770af42536f88
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2009-08-15 22:13:29 +0000

    /randomname now accepts "weapon".

--- trunk/scripts/rggNameGen.py
+++ trunk/scripts/rggNameGen.py
@@ -86,6 +86,43 @@ def _getJapaneseFullName(g=False):
   if g: return (_getJapaneseSurname() + " " + _getJapaneseMaleName())
   return (_getJapaneseSurname() + " " + _getJapaneseFemaleName())
 
+def _getArtifactWeaponName():
+  adj = ['black', 'white', 'great', 'dread', 'fell', 'dark', 'sacred', 'vorpal',
+         'unholy', 'bursting', 'shining', 'vaulting', 'holy', 'dead', 'eternal',
+         'red', 'green', 'blue', 'crimson', 'violet', 'iridescent', 'grand',
+         'supreme', 'titanic', 'screaming', 'ruby', 'sapphire', 'nightmare']
+  combine = ['bless', 'mourn', 'flare', 'death', 'god', 'doom', 'thorn', 'vine',
+             'ice', 'flame', 'hate', 'star', 'sun', 'moon', 'stone', 'earth',
+             'spark', 'poison', 'soul', 'mist']
+  post = ['blessing', 'mourning', 'winter', 'heaven', 'hell', 'doom', 'destruction',
+          'love', 'transcendent skill', 'might', 'cunning', 'sagacity', 'wisdom',
+          'sorrow', 'dawn', 'twilight', 'sunset', 'nightmare']
+  numbers = ['four', 'seven', 'nine', 'twelve', 'thirteen', 'hundred', 'thousand']
+  numpost = ['blessings', 'gods', 'winters', 'hells', 'heavens', 'sages',
+             'savants', 'wizards', 'archmagi', 'warlords', 'sunsets', 'dawns',
+             'dreams']
+  weaps = ['blade', 'cleaver', 'dagger', 'hammer', 'mace',
+           'sword', 'spear', 'lance', 'dirk', 'flail',
+           'axe', 'bow']
+  patterns = ["the $adj $comb$weap of $post",
+              "the $comb$weap",
+              "the $Weap of the $numb $numpost",
+              "the $adj $Weap of $post",
+              "the $adj $Weap of the $numb $numpost",
+              "the $comb$weap of the $numb $adj $numpost",
+              "the $Weap of the $numb $adj $numpost",
+              "the $adj $comb$weap",
+              "the $Weap of $numpost"]
+  pat = random.choice(patterns)
+  pat = pat.replace("$adj", random.choice(adj).capitalize())
+  pat = pat.replace("$comb", random.choice(combine).capitalize())
+  pat = pat.replace("$post", random.choice(post).capitalize())
+  pat = pat.replace("$numb", random.choice(numbers).capitalize())
+  pat = pat.replace("$numpost", random.choice(numpost).capitalize())
+  pat = pat.replace("$weap", random.choice(weaps))
+  pat = pat.replace("$Weap", random.choice(weaps).capitalize())
+  return pat
+
 def _generateAdvice():
   anto = [['+cold', '+heat'], ['wisdom', '+fool'], ['+light', '+darkness'],
         ['victory', 'defeat'], ['evil', 'good'], ['sound', 'silence']]
@@ -303,6 +340,8 @@ def getName(nametype):
       return (_getJapaneseFullName(True))
     elif nametype == "japanesefemalefull" or nametype == "japanesefull":
       return (_getJapaneseFullName())
+    elif nametype == "weapon":
+      return (_getArtifactWeaponName())
     else:
       return "Error McInvalidArgument"
   except:
