01f67f84da9b04c155d2422017924bc7c14b3e4c
kingoipo <kingoipo@4e94e682-459d-11de-b3c2-97ef4ed79301>
2010-01-07 23:40:11 +0000

    Fixed eraser function

--- trunk/src/Widgets/wGLWidget.cpp
+++ trunk/src/Widgets/wGLWidget.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 #include "wGLWidget.h"
+#include <cmath>
 
 #ifdef WIN32
   #define GL_TEXTURE_RECTANGLE_ARB GL_TEXTURE_2D //This abolishes POT textures, but at least it works! (god damn microsoft and its shitty products)
@@ -185,13 +186,83 @@ void wGLWidget::addLine(int x, int y, int w, int h)
     lines.append(QRect(x, y, w, h));
 }
 
+bool SegmentIntersectRectangle(double a_rectangleMinX,
+                               double a_rectangleMinY,
+                               double a_rectangleMaxX,
+                               double a_rectangleMaxY,
+                               double a_p1x,
+                               double a_p1y,
+                               double a_p2x,
+                               double a_p2y)
+{
+    // Find min and max X for the segment
+
+    double minX = a_p1x;
+    double maxX = a_p2x;
+
+    if(a_p1x > a_p2x)
+    {
+        minX = a_p2x;
+        maxX = a_p1x;
+    }
+
+    // Find the intersection of the segment's and rectangle's x-projections
+
+    if(maxX > a_rectangleMaxX)
+        maxX = a_rectangleMaxX;
+
+    if(minX < a_rectangleMinX)
+        minX = a_rectangleMinX;
+
+    if(minX > maxX) // If their projections do not intersect return false
+        return false;
+
+    // Find corresponding min and max Y for min and max X we found before
+
+    double minY = a_p1y;
+    double maxY = a_p2y;
+
+    double dx = a_p2x - a_p1x;
+
+    if(abs(dx) > 0.0000001)
+    {
+        double a = (a_p2y - a_p1y) / dx;
+        double b = a_p1y - a * a_p1x;
+        minY = a * minX + b;
+        maxY = a * maxX + b;
+    }
+
+    if(minY > maxY)
+    {
+        double tmp = maxY;
+        maxY = minY;
+        minY = tmp;
+    }
+
+    // Find the intersection of the segment's and rectangle's y-projections
+
+    if(maxY > a_rectangleMaxY)
+        maxY = a_rectangleMaxY;
+
+    if(minY < a_rectangleMinY)
+        minY = a_rectangleMinY;
+
+    if(minY > maxY) // If Y-projections do not intersect return false
+        return false;
+
+    return true;
+}
+
+
 void wGLWidget::deleteLine(int x, int y, int w, int h)
 {
-    QRect boundaries(x, y, w, h);
+    //QRect boundaries(x, y, w, h);
     uint i = 0;
     while(i < lines.size()) //I think that deleting an item from QVector makes the next item take the place of the deleted item, and thus having its place decremented by one.
     {
-        if(boundaries.contains(lines[i].topLeft()) || boundaries.contains(lines[i].bottomRight()))
+
+        //if(boundaries.contains(lines[i].topLeft()) || boundaries.contains(lines[i].bottomRight()))
+        if(SegmentIntersectRectangle(x, y, w, h, lines[i].x(), lines[i].y(), lines[i].width(), lines[i].height()))
         {
             cout << "Removing line : (" << lines[i].x() << ", " << lines[i].y() << ", " << lines[i].width() << ", " << lines[i].height() << ")" << endl;
             lines.remove(i);
