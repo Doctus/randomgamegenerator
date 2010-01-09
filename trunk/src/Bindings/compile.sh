ad1ab6b4a7db616bd25cf26156fd1aed3cce2c9f
kingoipo <kingoipo@4e94e682-459d-11de-b3c2-97ef4ed79301>
2010-01-09 02:02:54 +0000

    Added some safeguards to the linux compile script

--- trunk/src/Bindings/compile.sh
+++ trunk/src/Bindings/compile.sh
@@ -1,4 +1,12 @@
-moc bMain.h -o moc_bMain.cpp
+rm moc_bMain.cpp sip*
+
+if [ -f /usr/bin/moc-qt4 ]
+then
+	moc-qt4 bMain.h -o moc_bMain.cpp
+else
+	moc bMain.h -o moc_bMain.cpp
+fi
+
 python configure.py
 #g++ -shared -pipe -fPIC -O2 -Wall -g3 -ggdb -W -D_REENTRANT -DQT_CORE_LIB -DQT_GUI_LIB -I. -I/usr/include/python2.6 -lpython2.6 `pkg-config --cflags --libs QtGui` `pkg-config --cflags gl` ../../qt4/librandom-game-generator.so *.cpp -o _bmainmod.so
 g++ -shared -pipe -fPIC -O2 -Wall  -DQT_NO_DEBUG -DNDEBUG -W -D_REENTRANT -DQT_CORE_LIB -DQT_GUI_LIB -I. -I/usr/include/python2.6 -lpython2.6 `pkg-config --cflags --libs QtGui` `pkg-config --cflags gl` ../../qt4/librandom-game-generator.so *.cpp -o _bmainmod.so
