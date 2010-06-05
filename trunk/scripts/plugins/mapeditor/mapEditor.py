9a5a1886987e06e566b2b6c110e6133bda0c421b
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2010-06-05 23:59:36 +0000

    Assorted map editor improvements.

--- trunk/scripts/plugins/mapeditor/mapEditor.py
+++ trunk/scripts/plugins/mapeditor/mapEditor.py
@@ -18,12 +18,18 @@ class mapEditor(QtGui.QDockWidget):
         super(QtGui.QDockWidget, self).__init__(mainWindow)
         
         #self.__eat = True
+        self.painting = True
+        self.dragging = False
 
         self.setWindowTitle(self.tr("Map Editor"))
         self.widget = QtGui.QWidget(mainWindow)
         self.layout = QtGui.QBoxLayout(2)
         self.scrollarea = QtGui.QScrollArea(mainWindow)
+        self.togglePaintingButton = QtGui.QCheckBox(self.tr("Tile Painting"), mainWindow)
+        self.togglePaintingButton.setTristate(False)
+        self.togglePaintingButton.setCheckState(2)
         self.layout.addWidget(self.scrollarea)
+        self.layout.addWidget(self.togglePaintingButton)
         self.tilelabel = None
         self.widget.setLayout(self.layout)
         self.setWidget(self.widget)
@@ -31,22 +37,41 @@ class mapEditor(QtGui.QDockWidget):
 
         currentMap = rggViews._state.currentMap
         self.mapChangedResponse(currentMap)
+        
+        self.connect(self.togglePaintingButton, QtCore.SIGNAL('stateChanged(int)'), self.togglePainting)
 
         rggEvent.addMapChangedListener(self)
         rggEvent.addMousePressListener(self)
+        rggEvent.addMouseMoveListener(self)
         rggEvent.addMouseReleaseListener(self)
         
     def mousePressResponse(self, x, y, t):
-        if self.isVisible():
+        self.dragging = True
+        if self.isVisible() and self.painting:
             clickedtile = ((rggSystem.cameraPosition()[0] + x) / self.tilelabel.tilex,
                            (rggSystem.cameraPosition()[1] + y) / self.tilelabel.tiley)
             rggViews._state.currentMap.setTile(clickedtile, self.tilelabel.currentTile)
             rggViews.modifyCurrentMap()
             rggEvent.setEaten()
+
+    def mouseMoveResponse(self, x, y):
+        if self.isVisible() and self.painting and self.dragging:
+            clickedtile = ((rggSystem.cameraPosition()[0] + x) / self.tilelabel.tilex,
+                           (rggSystem.cameraPosition()[1] + y) / self.tilelabel.tiley)
+            if rggViews._state.currentMap.getTile(clickedtile) != self.tilelabel.currentTile:
+                rggViews._state.currentMap.setTile(clickedtile, self.tilelabel.currentTile)
+                rggViews.modifyCurrentMap()
+            rggEvent.setEaten()
             
     def mouseReleaseResponse(self, x, y, t):
-        if self.isVisible():
+        self.dragging = False
+        if self.isVisible() and self.painting:
             rggEvent.setEaten()
+            
+    def togglePainting(self, stat):
+        self.painting = stat
+        self.togglePaintingButton.setCheckState(stat)
+        self.dragging = False
 
     def mapChangedResponse(self, newMap):
         if newMap != None:
