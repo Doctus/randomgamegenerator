14a3369decbf8a5a95358b6442903e41ddf6a74e
kingoipo@gmail.com <kingoipo@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-05-26 21:17:14 +0000

    Made the linux build script more modular

--- trunk/module/build.sh
+++ trunk/module/build.sh
@@ -1,1 +1,1 @@
-g++ -O2 -fomit-frame-pointer -shared -fPIC -I /usr/include/python2.6/ `pkg-config --cflags --libs-only-L x11` glmod.c -lpython2.6 -lGL -o glmod.so
+g++ -O2 -fomit-frame-pointer -shared -fPIC `pkg-config --cflags --libs-only-L x11 python gl` glmod.c  -o glmod.so
