6cbc538a8030b3304df3034c41c8747178304d39
kingoipo@gmail.com <kingoipo@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-05-26 21:41:45 +0000

    Removed pesky windows line endings from the linux build file

--- trunk/module/build.sh
+++ trunk/module/build.sh
@@ -1,1 +1,1 @@
-g++ -O2 -fomit-frame-pointer -shared -fPIC `pkg-config --cflags --libs-only-L x11 python gl` glmod.c  -o glmod.so
+g++ -O2 -shared -fPIC `pkg-config --cflags --libs-only-L x11 python gl` glmod.c  -o glmod.so
