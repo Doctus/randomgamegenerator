f81f0d3381beca2c40922cf6ab56e660b6e8f8d8
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-06-07 03:12:51 +0000

    Added quasi-dubious personality generator

--- trunk/scripts/rggNameGen.py
+++ trunk/scripts/rggNameGen.py
@@ -401,6 +401,33 @@ def getAdvice():
   #We might want to do more here at some point, hence breaking it up.
   return _generateAdvice()
 
+personalitytraits = [["Argumentative","Garrulous","Hot-tempered","Overbearing","Articulate","Antagonistic","Quiet","Laconic","Soft-spoken","Secretive","Retiring","Mousy"],
+          ["Arrogant","Haughty","Elitist","Proud","Rude","Aloof"],
+          ["Capricious","Mischievous","Impulsive","Irreverent","Madcap"],
+          ["Careless","Thoughtless","Absent-minded","Dreamy","Insensitive"],
+          ["Courageous","Brave","Craven","Shy","Fearless","Obsequious"],
+          ["Curious","Inquisitive","Prying","Intellectual","Perceptive","Keen"],
+          ["Exacting","Perfectionist","Stern","Harsh","Punctual","Driven"],
+          ["Friendly","Trusting","Kind-hearted","Forgiving","Easy-going","Compassionate","Moody","Gloomy","Morose","Irritable","Vengeful"],
+          ["Greedy","Miserly","Hard-headed","Covetous","Avaricious","Thrifty","Generous","Wastrel","Spendthrift","Extravagant","Charitable"],
+          ["Naive","Honest","Truthful","Innocent","Gullible","Foolhardy"],
+          ["Opinionated","Bigoted","Biased","Narrow-minded","Blustering","Hide-bound"],
+          ["Optimistic","Cheerful","Happy","Diplomatic","Pleasant","Pessimistic","Fatalistic","Cynical","Sarcastic"],
+          ["Sober","Practical","Level-headed","Realistic","Dull","Reverent","Ponderous"],
+          ["Suspicious","Scheming","Paranoid","Cautious","Deceitful","Nervous"],
+          ["Uncivilized","Uncultured","Boorish","Barbaric","Graceless","Crude"],
+          ["Violent","Cruel","Sadistic","Immoral","Jealous"]]
+
+def _getPersonality():
+  types = random.sample(personalitytraits, 2)
+  trait1 = random.choice(types[0])
+  trait2 = random.choice(types[1])
+  pattern = random.choice(["and", "and", "and sometimes", "and slightly", "and rather"])
+  return " ".join((trait1, pattern, trait2))
+
+def getPersonality():
+  return _getPersonality()
+
 def _getKaleidoscope():
   blegh = random.choice([getName(random.choice(getName('kaikeys'))),
                          getName(random.choice(getName('kaikeys'))),
@@ -444,6 +471,7 @@ def getName(nametype):
              "french":_getFrenchRandomName,
              "macguffin":_getMacguffinName,
              "proverb":_getProverb,
+             "personality":_getPersonality,
              "kaijyuu":_getKaleidoscope}
   if nametype == "keys":
     return typedic.keys()
