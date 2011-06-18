f8f0ff6153b1ec23d770899610c8d3893706d99a
kingoipo@gmail.com <kingoipo@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-06-18 21:09:01 +0000

    Update the build files on other platforms as well.

--- trunk/module/build-windows.sh
+++ trunk/module/build-windows.sh
@@ -1,1 +1,1 @@
-g++ -O2 -fomit-frame-pointer -shared -D_WIN32 -I /C/Python27/include -L /C/Python27/libs glmod.c -lpython27 -lopengl32 -export-all-symbols -o glmod.pyd
+g++ -Wno-deprecated -O2 -fomit-frame-pointer -shared -D_WIN32 -I /C/Python27/include -L /C/Python27/libs glmod.c -lpython27 -lopengl32 -export-all-symbols -o glmod.pyd
