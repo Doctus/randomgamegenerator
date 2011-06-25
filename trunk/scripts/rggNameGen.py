cb4ad66125c870c436ca65b1d2aac0d7405587bd
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-06-25 21:14:51 +0000

    Added artifood generator.

--- trunk/scripts/rggNameGen.py
+++ trunk/scripts/rggNameGen.py
@@ -180,6 +180,39 @@ def _getMacguffinName(genre="fantasy"):
                                                    _getFrenchRandomName().capitalize()]))
     return result
   return "The Great MacGuffin"
+
+def getArtifood():
+  adj = ['black', 'white', 'great', 'dread', 'fell', 'dark', 'sacred', 'vorpal',
+         'unholy', 'bursting', 'shining', 'vaulting', 'holy', 'dead', 'eternal',
+         'red', 'green', 'blue', 'crimson', 'violet', 'iridescent', 'grand',
+         'supreme', 'titanic', 'screaming', 'ruby', 'sapphire', 'nightmare',
+         "ultimate", "endless", "lost",
+                  "forgotten", "ancient", "mystical", "arcane", "divine",
+                  "forbidden", "perfect", "brilliant"]
+  post = ['blessing', 'mourning', 'winter', 'heaven', 'hell', 'doom', 'destruction',
+          'love', 'transcendent skill', 'might', 'cunning', 'sagacity', 'wisdom',
+          'sorrow', 'dawn', 'twilight', 'sunset', 'nightmare',
+          "power", "wisdom", "undeath", "destruction", "life", "holiness",
+             "ice", "flames", "death", "silence", "immortality"]
+  numbers = ['four', 'seven', 'nine', 'twelve', 'thirteen', 'hundred', 'thousand']
+  numpost = ['blessings', 'gods', 'winters', 'hells', 'heavens', 'sages',
+             'savants', 'wizards', 'archmagi', 'warlords', 'sunsets', 'dawns',
+             'dreams']
+  patterns = ["the $adj $item of $post",
+              "the $item of the $numb $numpost",
+              "the $item of $post",
+              "the $adj $item of the $numb $numpost",
+              "the $item of the $numb $adj $numpost",
+              "the $item of the $numb $adj $numpost",
+              "the $adj $item",
+              "the $item of $numpost"]
+  pat = random.choice(patterns)
+  pat = pat.replace("$adj", random.choice(adj).capitalize())
+  pat = pat.replace("$post", random.choice(post).capitalize())
+  pat = pat.replace("$numb", random.choice(numbers).capitalize())
+  pat = pat.replace("$numpost", random.choice(numpost).capitalize())
+  pat = pat.replace("$item", getFood().capitalize())
+  return pat
     
 def _getProverb():
   patterns = ["$nadj $thing draws $entities",
@@ -685,6 +718,7 @@ def getName(nametype):
              "proverb":_getProverb,
              "personality":_getPersonality,
              "food":getFood,
+             "artifood":getArtifood,
              "kaijyuu":_getKaleidoscope}
   if nametype == "keys":
     return typedic.keys()
