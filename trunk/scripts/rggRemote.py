3697fe54f09d7a45f448c18663ed25a092698885
kirikayuumura.noir <kirikayuumura.noir@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-07-02 00:21:35 +0000

    Possibly fixed an issue with portrait transfer, still testing

--- trunk/scripts/rggRemote.py
+++ trunk/scripts/rggRemote.py
@@ -64,8 +64,7 @@ def sendSay(user, message):
 def respondICSay(chname, message, portrait):
     if len(portrait) > 1:
         portfile = makePortableFilename(os.path.join(PORTRAIT_DIR, portrait))
-        IAMTHEDOOMPHANTOM = rggResource.crm.translateFile(makePortableFilename(os.path.join(PORTRAIT_DIR, portrait)), rggResource.RESOURCE_IMAGE)
-        #^ Don't remember whether this is still needed for transfer etc.
+        rggResource.crm.listen(os.path.join(PORTRAIT_DIR, portrait), rggResource.RESOURCE_IMAGE, rggResource.crm, doNothing)
         ICSay(translate('remote', '<table><tr><td><img src="{port}" width="64" height="64"></td><td>{name}: {sayText}</td></tr></table><br />').format(
             port=portfile,                                                        
             name=linkedName(chname),
@@ -77,7 +76,6 @@ def respondICSay(chname, message, portrait):
 
 @clientRPC
 def sendICSay(user, message, chname, portrait):
-    rggResource.crm.listen(portrait, rggResource.RESOURCE_IMAGE, rggResource.crm, doNothing)
     respondICSay(allusers(), chname, message, portrait)
     
 def doNothing(blah, bleh, bloh):
