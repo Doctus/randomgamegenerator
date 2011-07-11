4e97cc09d071eb6e98dfd76c17db314644964ed9
kingoipo@gmail.com <kingoipo@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2011-07-11 12:27:59 +0000

    cjson does not have dumps

--- trunk/scripts/rggJson.py
+++ trunk/scripts/rggJson.py
@@ -21,10 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 from rggFields import validationError
 from rggSystem import makeLocalFilename
-try:
-    import cjson as json
-except:
-    import json
+import json
 
 def jsondumps(obj):
     """Dumps the object into a string. Contains no newlines."""
