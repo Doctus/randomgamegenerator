fd2c74d87b42b3f33e8c04365c4ca2f8701f7440
kirikayuumura.noir@gmail.com <kirikayuumura.noir@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2013-02-01 04:37:04 +0000

    Added a better generation method to the fantasyname generator (old one is still selectable with an argument)

--- scripts/generators/fantasyname.py
+++ scripts/generators/fantasyname.py
@@ -8,13 +8,24 @@ for word in words:
     if double.search(word) and len(word) < 9 and "lly" not in word[-4:] and "ed" not in word[-3:] and "ing" not in word[-4:]:
         double_letters.append(word)
 
+beginning = list(set('''jac mas will jayd n mich eth al aid dan anth matt josh andr jam dav log gabr sam nat luc land cal gav tyl luk jord brand jul aar jerem ang conn henr just aust rob soph isab emm oliv av emil abig madis m chl ell addis natal lil aver sof vic evel alex l amel z layl kat kayl alys alic sar ashl audr'''.split()))
+
+end = list(set('''ob on am en oah ael an er en el ew ah ua am ew es id in an er eph on iel uel ohn as ian an eb on aac in en er e an er as ah en att ert in i ia a y ail oe eth ie ey yn'''.split()))
+        
 def singlify(matchobj):
     return matchobj.group(0)[0]
+
+def _getParts():
+    return (random.choice(beginning) + random.choice(end)).title()
         
-def _getFantasy():
+def _getDouble():
     word = random.choice(double_letters)
     return double.sub(singlify, word[:-2]).title()
     
 def getName(args):
-    if args == "help": return "Generates a fantasy-ish name by removing a double letter from an actual word."
-    return _getFantasy()
+    if args == "help": return "Generates a fantasy-ish name by: combining parts of two real names ('parts') or removing a double letter from an actual word ('double')."
+    elif args == "double":
+        return _getDouble()
+    elif args == "parts":
+        return _getParts()
+    return _getParts()
