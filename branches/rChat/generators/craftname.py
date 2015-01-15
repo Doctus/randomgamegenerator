86a25ee2925c9c27fc3410699d6dfa22ec8ca7da
kirikayuumura.noir@gmail.com <kirikayuumura.noir@gmail.com@4e94e682-459d-11de-b3c2-97ef4ed79301>
2015-01-15 17:42:23 +0000

    Removed some old crap

--- branches/rChat/generators/craftname.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def _getCraftName():
-    parts = 'Wolf Raven Silver Moon Star Water Snow Sea Tree Wind Cloud Witch Thorn Leaf White Black Green Fire Rowan Swan Night Red Mist Hawk Feather Eagle Song Sky Storm Sun Wood'.split()
-    return random.choice((" ".join(random.sample(parts, 2)), " ".join(random.sample(parts, 2))+random.choice(parts).lower()))
-    
-def getName(args):
-    if args == "help": return "Generates a craft name in the style of Lady Pixie Moondrip."
-    return _getCraftName()
--- branches/rChat/generators/flower.py
+++
@@ -1,9 +0,0 @@
-﻿import random
-
-def getFlower():
-    flower = ["Acacia","Acanthus","Aconite","Agrimony","Aloe","Almond","Amaranth","Amaryllis","Ambrosia","Anemone","Angrec","Apple blossom","Arborvitae","Arbutus","Arum","Asparagus","Asphodel","Aster","Azalea","Baby's breath","Bachelor button","Balm","Balsam","Balsamine","Bay wreath","Begonia","Bellflower","Bells of Ireland","Bird's-foot Trefoil","Bird-of-Paradise","Box","Broom","Bulrush","Bumblebee Orchid","Buttercup","Cabbage","Camellia japonica","Campanula","Canterbury Bells","Carnation","Celandine","Cherry","Chestnut","China aster","Chrysanthemum","Coreopsis","Cowslip","Clover","Coriander","Cypress","Daffodil","Dahlia","Daisy","Dandelion","Delphinium","Eglantine Rose","Elderflower","Fennel","Forget-me-not","Fungus","Gardenia","Geranium","Gorse","Grass","Heliotrope","Hibiscus","Hollyhock","Honeysuckle","Houseleek","Hydrangea","Iris","Ivy","Jonquil","Laurestine","Lavender","Lemon","Lettuce","Lichen","Lilac","Lily","Lily of the Valley","Lime","Lobelia","Lotus","Magnolia","Mallow","Marigold","Mayflower","Mignonette","Mint","Moonflower","Morning glory","Mullein","Nasturtium","Oak leaf","Oats","Olive","Orchid","Oxeye daisy","Peach","Pear","Peony","Phlox","Plum","Plumeria","Primrose","Poppy","Rose","Rosemary","Rue","Snowdrop","Straw","Sunflower","Sweetbrier","Thorn-apple","Thistle","Thyme","Tulip-tree","Tulip","Viscaria","Willow","Witch-hazel","Wheat"]
-    return random.choice(flower)
-
-def getName(args):
-    if args == "help": return "Generates a flower. No special arguments."
-    return getFlower()
