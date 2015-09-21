ae9661dd468d7e958360b36292734237b970d5e6
kaijyuu2 <kaijyuu2@gmail.com>
2015-09-21 15:58:42 -0600

    added "suddenly" generator

--- libraries/generators/__init__.py
+++ libraries/generators/__init__.py
@@ -1,8 +1,9 @@
+__all__ = ['advice', 'artifood', 'birthday', 'disease', 'dwarf', 'eviltitle', 'familyname', 'fantasyname', 'flower', 'food', 'french', 'inn', 'japanese', 'korean', 'macguffin', 'magicollect', 'manerator', 'normalname', 'personality', 'pony', 'proverb', 'relationship', 'suddenly', 'technique', 'weapon']
+# Don't modify the line above, or this line!
 try:
-	__all__ = ['advice', 'artifood', 'birthday', 'disease', 'dwarf', 'eviltitle', 'familyname', 'fantasyname', 'flower', 'food', 'french', 'inn', 'japanese', 'korean', 'macguffin', 'magicollect', 'manerator', 'normalname', 'personality', 'pony', 'proverb', 'relationship', 'technique', 'weapon']
-	# Don't modify the line above, or this line!
 	import automodinit
 	automodinit.automodinit(__name__, __file__, globals())
 	del automodinit
-except Exception:
+except Exception as e:
+	print(e)
 	from . import advice, artifood, birthday, disease, dwarf, eviltitle, familyname, fantasyname, flower, food, french, inn, japanese, korean, macguffin, magicollect, manerator, normalname, personality, pony, proverb, relationship, technique, weapon
